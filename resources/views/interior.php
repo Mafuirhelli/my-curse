f6c449443b5c7e15927f460309ea83a2316c11f9
Mafuirhelli <61899009+Mafuirhelli@users.noreply.github.com>
2025-10-22 20:00:53 +0700

    import pages from cursovaya + reinstalling project

---
+++ resources/views/interior.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Интерьер</title>
+    <link rel="stylesheet" href="../../../../Загрузки/curseovaya--main/css/header.css">
+    <link rel="stylesheet" href="../../../../Загрузки/curseovaya--main/css/footer.css">
+    <link rel="stylesheet" href="../../../../Загрузки/curseovaya--main/css/fonts.css">
+    <link rel="stylesheet" href="../../../../Загрузки/curseovaya--main/css/interior.css">
+    <link rel="stylesheet" href="../../../../Загрузки/curseovaya--main/css/style.css">
+</head>
+<body class="alt-bc-color">
+<header>
+    <div class="logo">
+        <img class="logo-icon" src="../../../../Загрузки/curseovaya--main/images/svg/svg_icons/logo.svg" alt="Лого">
+        <a href="main.html">mugduck</a>
+    </div>
+
+    <nav>
+        <input id="burger-toggle" type="checkbox">
+        <label for="burger-toggle">
+            <span></span>
+        </label>
+        <ul class="menu">
+            <li><a href="menu.html">Меню</a></li>
+            <li><a href="interior.html">Интерьер</a></li>
+            <li><a href="contact.php">Контакты</a></li>
+            <li><a href="menu.html#delivery-category">Доставка</a></li>
+        </ul>
+    </nav>
+    <a class="login-button" href="#">Войти</a>
+</header>
+<main>
+    <section class="intro">
+        <h1>Наше заведение</h1>
+    </section>
+    <section>
+        <div class="interior-content-row">
+            <img src="../../../../Загрузки/curseovaya--main/images/interior_page/1.png" alt="placeholder">
+            <div class="interior-content-row-text">
+                <div class="headline">
+                    <h4>aasadsf</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi.
+            </div>
+        </div>
+        <div class="interior-content-row">
+            <img src="../../../../Загрузки/curseovaya--main/images/interior_page/2.png" alt="placeholder">
+            <div class="interior-content-row-text">
+                <div class="headline">
+                    <h4>aasadsf</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi.
+            </div>
+        </div>
+        <div class="interior-content-row reverse">
+            <img src="../../../../Загрузки/curseovaya--main/images/interior_page/2.png" alt="placeholder">
+            <div class="interior-content-row-text">
+                <div class="headline">
+                    <h4>aasadsf</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi.
+            </div>
+        </div>
+        <div class="interior-content-row">
+            <img src="../../../../Загрузки/curseovaya--main/images/interior_page/2.png" alt="placeholder">
+            <div class="interior-content-row-text">
+                <div class="headline">
+                    <h4>aasadsf</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi.
+            </div>
+        </div>
+        <div class="interior-content-row reverse">
+            <img src="../../../../Загрузки/curseovaya--main/images/interior_page/2.png" alt="placeholder">
+            <div class="interior-content-row-text">
+                <div class="headline">
+                    <h4>aasadsf</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et in sed in pellentesque ornare nunc nisl. Augue habitant accumsan, ut parturient orci ac etiam congue mi.
+            </div>
+        </div>
+    </section>
+</main>
+
+<footer>
+    <div class="footer-column"><p class="small-text">© 2021 — 2024</p></div>
+    <div class="footer-column">
+        <h5>Мы в социальных сетях</h5>
+        <a href="https://vk.com" class="small-text">Вк</a>
+        <a href="https://bsky.app" class="small-text">Блюскай</a>
+        <a href="https://web.telegram.org" class="small-text">Телеграм</a>
+    </div>
+    <div class="footer-column">
+        <h5>Контакты</h5>
+        <a href="#" class="small-text">г. Томск</a>
+        <a href="#" class="small-text">пр. Кирова 69</a>
+        <a href="tel:88005353535" class="small-text">+ 8 800 535 35 35</a>
+    </div>
+    <div class="footer-column">
+        <h5>Часы Работы</h5>
+        <a href="#" class="small-text">Пн-Вс 10:00-00:00</a>
+    </div>
+    <div class="footer-column">
+        <h5>Прочее</h5>
+        <a href="#" class="small-text">Банкеты</a>
+        <a href="#" class="small-text">Корпоративы</a>
+        <a href="#" class="small-text">Праздники</a>
+    </div>
+</footer>
+</body>
+</html>
---
+++ views/menu.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Меню</title>
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/menu.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body class="bc-color">
+<header>
+    <div class="logo">
+        <img class="logo-icon" src="../images/svg/svg_icons/logo.svg" alt="Лого">
+        <a href="main.php">mugduck</a>
+    </div>
+
+    <nav>
+        <input id="burger-toggle" type="checkbox">
+        <label for="burger-toggle">
+            <span></span>
+        </label>
+        <ul class="menu">
+            <li><a href="menu.php">Меню</a></li>
+            <li><a href="interior.php">Интерьер</a></li>
+            <li><a href="contact.php">Контакты</a></li>
+            <li><a href="menu.php#delivery-category">Доставка</a></li>
+        </ul>
+    </nav>
+    <a class="login-button" href="#">Войти</a>
+</header>
+<main>
+    <section class="intro">
+        <h1>Посмотрите на наше <span>Меню</span></h1>
+        <p class="subtitle">Оно прекрасное, вкусное и вообще класс, всем пробовать, да!</p>
+    </section>
+    <section class="menu-category">
+        <h2>Рамен</h2>
+        <p class="subtitle">Это Рамен, такая лапша с топингами, очень вкусно, всем нравится</p>
+        <div class="menu-category-content">
+            <div class="category-cards">
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <img src="../images/menu_page/celery_egg_ramen.png" alt="Рамен" class="category-card-img">
+        </div>
+    </section>
+    <section class="menu-category">
+        <h2>Напитки</h2>
+        <p class="subtitle">Если хотите утолить вашу жажду - дерзайте, тут подают легендарный пилк</p>
+        <div class="menu-category-content">
+            <div class="category-cards">
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <img src="../images/menu_page/pilk_drink.png" alt="Пилк" class="category-card-img">
+        </div>
+    </section>
+    <section class="menu-category">
+        <h2>Мясо</h2>
+        <p>Мясо, мясо мясо, кто не любит мясо? Мясо любят все</p>
+        <div class="menu-category-content">
+            <div class="category-cards">
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <img src="../images/menu_page/beef_meat.png" alt="Говядина" class="category-card-img">
+        </div>
+    </section>
+    <section class="menu-category">
+        <h2>Салаты</h2>
+        <p class="subtitle">Для тех кто не любит мясо</p>
+        <div class="menu-category-content">
+            <div class="category-cards">
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div class="category-card">
+                    <h3 class="category-card-price">20<span class="rouble">g</span></h3>
+                    <h3 class="category-card-name">Deep Sea Snow White Cod Fillet</h3>
+                    <p class="category-card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <img src="../images/menu_page/avocado_mozzarella_salad.png" alt="Моццарелла салат" class="category-card-img">
+        </div>
+    </section>
+    <section class="delivery-section">
+        <div class="delivery-section-title">
+            <h2>Оформить доставку</h2>
+            <p class="subtitle">От количества человек зависит количество прибовров</p>
+        </div>
+        <form class="menu-delivery">
+            <input class="form-control" type="text" placeholder="Рамен" id="delivery-category">
+            <input class="form-control" type="time" value="09:00" id="delivery-time">
+            <select class="form-control">
+                <option value="1">1 Человек</option>
+                <option value="2" selected>2 Человека</option>
+                <option value="3">3 Человек</option>
+                <option value="4">4 Человека</option>
+                <option value="5">5 Человек</option>
+                <option value="6">Более 5 Человек</option>
+            </select>
+        </form>
+        <button class="primary-btn">Заказать</button>
+    </section>
+</main>
+<footer>
+    <div class="footer-column"><p class="small-text">© 2021 — 2024</p></div>
+    <div class="footer-column">
+        <h5>Мы в социальных сетях</h5>
+        <a href="https://vk.com" class="small-text">Вк</a>
+        <a href="https://bsky.app" class="small-text">Блюскай</a>
+        <a href="https://web.telegram.org" class="small-text">Телеграм</a>
+    </div>
+    <div class="footer-column">
+        <h5>Контакты</h5>
+        <a href="#" class="small-text">г. Томск</a>
+        <a href="#" class="small-text">пр. Кирова 69</a>
+        <a href="tel:88005353535" class="small-text">+ 8 800 535 35 35</a>
+    </div>
+    <div class="footer-column">
+        <h5>Часы Работы</h5>
+        <a href="#" class="small-text">Пн-Вс 10:00-00:00</a>
+    </div>
+    <div class="footer-column">
+        <h5>Прочее</h5>
+        <a href="#" class="small-text">Банкеты</a>
+        <a href="#" class="small-text">Корпоративы</a>
+        <a href="#" class="small-text">Праздники</a>
+    </div>
+</footer>
+</body>
+</html>
